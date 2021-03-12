b34bd38d6441a3aecedf0b472be8caa295af827b
Adam Dixon <adam@pfl.com>
2021-03-11 21:00:55 -0700

    Copy DLLs

--- PostDeploy.sh
+++ PostDeploy.sh
@@ -1,3 +1,6 @@
+sudo cp DLLs/PM3DDICP.dll /lib/PM3DDICP.dll
+sudo cp DLLs/PM3CsafeCP.dll /lib/PM3CsafeCP.dll
+sudo cp DLLs/PM3USBCP.dll /lib/PM3USBCP.dll
 sudo cp Erg.service /etc/systemd/system/Erg.service
 sudo systemctl daemon-reload
 sudo systemctl start Erg
