4e2953574f725ae2c6515a06c91053a1a2b3a287
Adam Dixon <adam@pfl.com>
2021-03-14 15:49:11 -0600

    Remove default dll reference

--- LibUsbDotNet/LibUsbDotNet/Generated/NativeMethods.cs
+++ LibUsbDotNet/LibUsbDotNet/Generated/NativeMethods.cs
@@ -54,9 +54,6 @@ namespace LibUsbDotNet
 #elif REFERENCE_ASSEMBLY
                 internal const string LibUsbNativeLibrary = "usb";
                 internal const CallingConvention LibUsbCallingConvention = CallingConvention.Cdecl;
-#elif true
-        internal const string LibUsbNativeLibrary = "libusb-1.0.dll";
-        internal const CallingConvention LibUsbCallingConvention = CallingConvention.StdCall;
 #endif
 
         [DllImport(LibUsbNativeLibrary, CallingConvention = LibUsbCallingConvention, EntryPoint = "libusb_init")]
