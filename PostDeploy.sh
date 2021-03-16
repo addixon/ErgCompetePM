c22a62d3eb211d35126a39e75dcaeff20a659b92
Adam Dixon <adam@pfl.com>
2021-03-15 20:52:45 -0600

    Leverage systemd instead of rc

--- PostDeploy.sh
+++ PostDeploy.sh
@@ -1,7 +1,6 @@
 sudo systemctl stop Erg
 sudo rm -f /etc/systemd/system/Erg.service
 sudo cp Erg.service /etc/systemd/system/Erg.service
-sudo update-rc.d Erg defaults
-sudo update-rc.d Erg enable
+sudo systemctl enable Erg
 sudo systemctl daemon-reload
 sudo chmod +x ErgCompetePM;
