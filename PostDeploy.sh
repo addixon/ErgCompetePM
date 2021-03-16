0d4ad4d54709fbc0f8a02da602268374f31a6c53
Adam Dixon <adam@pfl.com>
2021-03-15 21:01:06 -0600

    Rearrange postdeploy commands

--- PostDeploy.sh
+++ PostDeploy.sh
@@ -1,6 +1,6 @@
 sudo systemctl stop Erg
 sudo rm -f /etc/systemd/system/Erg.service
 sudo cp Erg.service /etc/systemd/system/Erg.service
-sudo systemctl enable Erg
-sudo systemctl daemon-reload
 sudo chmod +x ErgCompetePM;
+sudo systemctl daemon-reload
+sudo systemctl enable Erg
